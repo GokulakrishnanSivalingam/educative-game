22ef2fb19d02264921683020083057179ae6b2fc
1234gk <144155567+1234gk@users.noreply.github.com>
2024-10-18 15:29:03 +0530

    changes in frontend

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { MdOutlineLogin } from "react-icons/md";
+import {Link } from "react-router-dom"
 
 
 
@@ -16,13 +17,13 @@ function Navbar() {
     <nav className="navbar">
       <div className="logo">AVLearn</div>
       <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
-        <li> <a href="/">Home</a></li>
-        <li> <a to="/">About</a></li>
-        <li><a to="/feedback">Feedback</a></li>
+        <li> <Link to="/">Home</Link></li>
+        <li> <Link to="/">About</Link></li>
+        <li><Link to="/feedback">Feedback</Link></li>
         <li><a href="/register">Register</a></li>
     
  
- <div className="log"><li><a href='/login'><MdOutlineLogin/></a></li></div>
+ <div className="log"><li><Link to='/login'><MdOutlineLogin/></Link></li></div>
       </ul>
       <div className="hamburger" onClick={toggleMenu}>
         <span className="bar"></span>
